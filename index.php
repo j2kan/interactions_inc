2d2b360c9ace4d5e893b639003a7cfb821e20b71
Johnson Kan <j2kan@uwaterloo.ca>
2016-11-18 17:09:46 -0500

    k

--- index.php
+++ index.php
@@ -1,4 +1,69 @@
+<link rel="stylesheet" href="Homepage/components/bootstrap2/css/bootstrap.css">
+<link rel="stylesheet" href="Homepage/components/bootstrap2/css/bootstrap-responsive.css">
+	
 <style>
+
+.nav ul {
+  list-style: none;
+  background-color: #444;
+  text-align: left;
+  padding: 0;
+  margin: -10px;
+}
+.nav li {
+  font-family: 'Oswald', sans-serif;
+  font-size: 1.2em;
+  line-height: 40px;
+  height: 40px;
+  border-bottom: 1px solid #888;
+}
+ 
+.nav a {
+  text-decoration: none;
+  color: #fff;
+  display: block;
+  transition: .3s background-color;
+}
+ 
+.nav a:hover {
+  background-color: #005f5f;
+}
+ 
+.nav a.active {
+  background-color: #fff;
+  color: #444;
+  cursor: default;
+}
+ 
+@media screen and (min-width: 600px) {
+  .nav li {
+    width: 120px;
+    border-bottom: none;
+    height: 50px;
+    line-height: 50px;
+    font-size: 1.4em;
+  }
+ 
+  /* Option 1 - Display Inline */
+  .nav li {
+    display: inline-block;
+    margin-right: -75px;
+  }
+ 
+  /* Options 2 - Float
+  .nav li {
+    float: left;
+  }
+  .nav ul {
+    overflow: auto;
+    width: 600px;
+    margin: 0 auto;
+  }
+  .nav {
+    background-color: #444;
+  }
+  */
+}
 /* Bordered form */
 form {
     border: 3px solid #f1f1f1;
@@ -40,7 +105,7 @@ button {
 
 /* Avatar image */
 img.avatar {
-    width: 40%;
+    width: 30%;
     border-radius: 50%;
 }
 
@@ -52,7 +117,7 @@ img.avatar {
 /* The "Forgot password" text */
 span.psw {
     float: right;
-    padding-top: 16px;
+    padding-top: 0;
 }
 
 /* Change styles for span and cancel button on extra small screens */
@@ -66,25 +131,70 @@ span.psw {
     }
 }
 </style>
-<form action="action_page.php">
-  <div class="imgcontainer">
-    <img src="johnson.jpg" alt="Avatar" class="avatar">
-  </div>
+<style>
+  html, body {
+    height: 100%;
+  }
+  #tableContainer-1 {
+    height: 100%;
+    width: 100%;
+    display: table;
+  }
+  #tableContainer-2 {
+    vertical-align: middle;
+    display: table-cell;
+    height: 100%;
+  }
+  #myTable {
+    margin: 0 auto;
+  }
 
+</style>
+<body class="news">
+  <header>
+    <div class="nav">
+      <ul>
+      --- 
+      	<li> <img src="connect.jpg" alt="Avatar" class="avatar"> </li>
+        <li class="home"><a href="#">Connect</a></li>
+        <!-- <li class="tutorials"><a href="#">Tutorials</a></li> -->
+        <!-- <li class="about"><a href="#">About</a></li> -->
+        <!-- <li class="news"><a href="#">Newsletter</a></li> -->
+        <!-- <li class="contact"><a href="#">Contact</a></li> -->
+        <li> </li>
+      </ul>
+    </div>
+  </header>
+</body>
+
+<div id="tableContainer-1">
+  <!-- <div id="tableContainer-2"> -->
+  <p><center> Weather? </center></p>
+    <table id="myTable" border>
+      <tr><td>
+  <form action="action_page.php">
+  <div class="imgcontainer">
+  <h1> Sign in </h1>
+  </div>
   <div class="container">
     <label><b>Username</b></label>
     <input type="text" placeholder="Enter Username" name="uname" required>
 
     <label><b>Password</b></label>
     <input type="password" placeholder="Enter Password" name="psw" required>
-
-    <button type="submit">Login</button>
+    <button type="submit">Connect</button>
     <input type="checkbox" checked="checked"> Remember me
+    <span class="psw"> <a href="#">Trouble connecting?</a></span>
   </div>
+	</form>
+      </td></tr>
+    </table>
+  <!-- </div> -->
+</div>
 
-  <div class="container" style="background-color:#f1f1f1">
-    <button type="button" class="cancelbtn">Cancel</button>
-    <span class="psw">Forgot <a href="#">password?</a></span>
-  </div>
-</form
+
+
+
+
+
 
