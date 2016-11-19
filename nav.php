fc05462e761b100152fca636f1b908736a19425d
Johnson Kan <j2kan@uwaterloo.ca>
2016-11-19 12:02:38 -0500

    k

---
+++ nav.php
@@ -0,0 +1,37 @@
+<link rel="stylesheet" href="Homepage/components/bootstrap3/css/bootstrap.css">
+<link rel="stylesheet" href="Homepage/components/bootstrap3/css/bootstrap-responsive.css">
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>Interaction Inc</title>
+</head>
+
+  <nav class="navbar navbar-inverse">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <a class="navbar-brand" href="#"> <span class="glyphicon glyphicon-globe"></span>   Connect</a>
+    </div>
+    <ul class="nav navbar-nav">
+    </ul>
+    <ul class="nav navbar-nav navbar-right">
+      <li><a href="#"><span class="glyphicon glyphicon-map-marker"></span>Events</a></li>
+      <li><a href="#"><span class="glyphicon glyphicon-pencil"></span>Class</a></li>
+      <li><a href="#"><span class="glyphicon glyphicon-usd"></span>Jobmine</a></li>
+      <li><a href="#"><span class="glyphicon glyphicon-envelope"></span>Email</a></li>
+      <li><a href="#"><span class="glyphicon glyphicon-log-out"></span>Logout</a></li>
+    </ul>
+  </div>
+</nav>
+
+
+
+
+<footer>
+<!-- <div id="plemx-root"></div>  -->
+<br>
+<br>
+ <center>© 2015-2016 Interactions Inc.</center>
+</footer>
+</html>
