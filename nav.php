d43e1742fe74aefc3202e8fd976f10bae92c66cd
Johnson Kan <j2kan@uwaterloo.ca>
2016-11-19 14:05:41 -0500

    fix ..

--- nav.php
+++ nav.php
@@ -16,18 +16,18 @@ img.avatar {
   <nav class="navbar navbar-inverse">
   <div class="container-fluid">
     <div class="navbar-header">
-      <a class="navbar-brand" href="/interactions_inc/homepage/home.php"> <span class="glyphicon glyphicon-globe"></span>   Connect </a>
+      <a class="navbar-brand" href="../homepage/home.php"> <span class="glyphicon glyphicon-globe"></span>   Connect </a>
     </div>
     <ul class="nav navbar-nav">
     </ul>
     <ul class="nav navbar-nav navbar-right">
     <li> <a href="../underdev/under_dev.html"> <img src="../rudra2.jpg" class="avatar" align="right"> </a>
     </li>
-      <li><a href="/interactions_inc/homepage/home.php"><span class="glyphicon glyphicon-pencil"></span> Class</a></li>
-      <li><a href="/interactions_inc/homepage/JobmineSelectScreen1.php"><span class="glyphicon glyphicon-usd"></span>Jobmine</a></li>
-      <li><a href="/interactions_inc/eventPage/events.php"><span class="glyphicon glyphicon-map-marker"></span> Events</a></li>
+      <li><a href="../homepage/home.php"><span class="glyphicon glyphicon-pencil"></span> Class</a></li>
+      <li><a href="..//homepage/JobmineSelectScreen1.php"><span class="glyphicon glyphicon-usd"></span>Jobmine</a></li>
+      <li><a href="..//eventPage/events.php"><span class="glyphicon glyphicon-map-marker"></span> Events</a></li>
       <li><a href="../underdev/under_dev.html"><span class="glyphicon glyphicon-envelope"></span> Email</a></li>
-      <li><a href="/interactions_inc/"><span class="glyphicon glyphicon-log-out"></span> Logout</a></li>
+      <li><a href="../index.php"><span class="glyphicon glyphicon-log-out"></span> Logout</a></li>
     </ul>
   </div>
 </nav>
