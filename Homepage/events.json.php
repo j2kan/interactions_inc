90cfdf5305f230a31e63295e3c1b4877d9eaa403
lauradobson <ldobson@uwaterloo.ca>
2016-11-19 12:01:33 -0500

    Included under dev page

--- Homepage/events.json.php
+++ Homepage/events.json.php
@@ -4,7 +4,7 @@
 		{
 			"id": "293",
 			"title": "MSCI 343 Lecture",
-			"url": "http://www.example.com/",
+			"url": "../underdev/under_dev.html",
 			"class": "event-important",
 			"start": "1479483007000",
 			"end":   "1479504607000"
@@ -12,7 +12,7 @@
 		{
 			"id": "256",
 			"title": "MSCI 342 Assignment Due",
-			"url": "http://www.example.com/",
+			"url": "../underdev/under_dev.html",
 			"class": "event-special",
 			"start": "1479448740000",
 			"end":   "1479448740000"
@@ -20,7 +20,7 @@
 		{
 			"id": "276",
 			"title": "MSCI 332 Lecture",
-			"url": "http://www.example.com/",
+			"url": "../underdev/under_dev.html",
 			"class": "event-important",
 			"start": "1479308400000",
 			"end":   "1479315000000"
@@ -28,7 +28,7 @@
 		{
 			"id": "294",
 			"title": "MSCI 446 Lecture",
-			"url": "http://www.example.com/",
+			"url": "../underdev/under_dev.html",
 			"class": "event-important",
 			"start": "1479220200000",
 			"end":   "1479223200000"
@@ -36,7 +36,7 @@
 		{
 			"id": "297",
 			"title": "MSCI 446 Tutorial",
-			"url": "http://www.example.com/",
+			"url": "../underdev/under_dev.html",
 			"class": "event-important",
 			"start": "1479249000000",
 			"end":   "1479252000000"
@@ -44,7 +44,7 @@
 		{
 			"id": "54",
 			"title": "Info Tesla Interview",
-			"url": "http://www.example.com/",
+			"url": "../underdev/under_dev.html",
 			"class": "event-info",
 			"start": "1479567600000",
 			"end":   "1479571200000"
@@ -52,7 +52,7 @@
 		{
 			"id": "53",
 			"title": "Important",
-			"url": "http://www.example.com/",
+			"url": "../underdev/under_dev.html",
 			"class": "event-important",
 			"start": "1479567600000",
 			"end":   "1479571200000"
@@ -60,7 +60,7 @@
 		{
 			"id": "59",
 			"title": "Success",
-			"url": "http://www.example.com/",
+			"url": "../underdev/under_dev.html",
 			"class": "event-success",
 			"start": "1479567600000",
 			"end":   "1479571200000"
@@ -68,7 +68,7 @@
 		{
 			"id": "61",
 			"title": "Inverse",
-			"url": "http://www.example.com/",
+			"url": "../underdev/under_dev.html",
 			"class": "event-inverse",
 			"start": "1479567600000",
 			"end":   "1479571200000"
@@ -76,7 +76,7 @@
 		{
 			"id": "61",
 			"title": "Warning",
-			"url": "http://www.example.com/",
+			"url": "../underdev/under_dev.html",
 			"class": "event-warning",
 			"start": "1479567600000",
 			"end":   "1479571200000"
@@ -84,7 +84,7 @@
 		{
 			"id": "532",
 			"title": "Facebook Interview",
-			"url": "http://www.example.com/",
+			"url": "../underdev/under_dev.html",
 			"class": "event-info",
 			"start": "1478793600000",
 			"end":   "1478797200000"
@@ -92,7 +92,7 @@
 		{
 			"id": "548",
 			"title": "MSCI 343: Assignment 3 Due",
-			"url": "http://www.example.com/",
+			"url": "../underdev/under_dev.html",
 			"class": "event-special",
 			"start": "1479574800000",
 			"end":   "1479574800000"
@@ -100,7 +100,7 @@
 		{
 			"id": "295",
 			"title": "MSCI 333 Group Meeting",
-			"url": "http://www.example.com/",
+			"url": "../underdev/under_dev.html",
 			"class": "event-warning",
 			"start": "1479650400000",
 			"end":   "1479682800000"
